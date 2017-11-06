ac5db2c18e550a054c0df43ac1f55770b13fc1ee
edgar amendi <omindeame@gmail.com>
2017-11-06 11:59:35 +0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,60 @@
-var pingPong = function(number) {
-  if ((number % 3 === 0) || (number % 5 === 0)) {
-    return true;
-  } else {
-    return false;
-  };
-};
+$(document).ready(function () {
+
+	var listnumber=[];
+	var index;
+
+	var btnsubmit = $("#btnpress");
+	var inputnum = $("input#number");
+	var pwarning =$("p#warning");
+	var ul_nums = $("ul#returnednums");
+	inputnum.click(function() {
+		/* Act on the event */
+			$(this).val("");
+		ul_nums.html("<li style='color:green;'>Adding new number</li>");
+
+	});
+	btnsubmit.click(function() {
+		ul_nums.html("");
+		if (inputnum.val()!="") {
 
 
-$(document).ready(function() {
-  $("form#formInput").submit(function(event) {
-    $("ul").empty();
-    var number = parseInt($("input#numberEntry").val());
+			// console.log(inputnum.val());
+			// console.log(parseInt(inputnum.val()));
 
-    for (i = 1; i <= number; i++)
-      if (i % 3 === 0 && i % 5 === 0) {
-        $("ul").append("<li>pingpong</li>");
+			for (index=0;index<parseInt(inputnum.val())+1;index++){
+				listnumber.push(index);
+				// console.log(listnumber);
 
-      } else if (i % 3 === 0) {
-      $("ul").append("<li>ping</li>");
+				ul_nums.prepend("<li>"+ping(listnumber[index])+"</li>");
 
-    } else if (i % 5 === 0) {
-      $("ul").append("<li>pong</li>");
+			}
 
-    } else {
-      $("ul").append("<li>" + i + "</li>");
-    }
-    event.preventDefault();
-  });
+			console.log(listnumber);
+			console.log(ping(3));
+
+		}else{
+			pwarning.html("fill in the input field with a number");
+			inputnum.css({
+				border: '1px solid red',
+				outline: 'red',
+				padding:"2px",
+				textIndent :'10px'
+			});
+		}
+	});
+
+	function ping (num) {
+		if (num%15==0) {
+			return "pingpong";
+		}else if (num%5==0){
+			return "ping";
+		}else if(num%3==0){
+			return "ping";
+		}else{
+			return num;
+		}
+	}
+
+
+
 });
